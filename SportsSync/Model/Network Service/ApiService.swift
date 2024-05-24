75c88f76876ccda2fb40c9f002060dc0f708cd5f
Aya-moustafa <aiyamoustafa910@gmail.com>
2024-05-24 20:43:12 +0300

    Finish from fetch data from Api and handle ui of the home screen

--- SportsSync/Model/Network Service/ApiService.swift
+++ SportsSync/Model/Network Service/ApiService.swift
@@ -4,24 +4,31 @@
 //
 //  Created by JETSMobileLabMini5 on 11/05/2024.
 //
-
+//
 import Foundation
 import Alamofire
-struct ApiResponse<T: Codable>: Codable {
-    let success: Int
-    let result: [T]
-}
-class ApiService : NetworkProtocol {
-    static let shared = ApiService()
-    func fetchData<T: Codable>(urlString: String, completion: @escaping (Result<[T], Error>) -> Void) {
-        AF.request(urlString).responseDecodable(of: ApiResponse<T>.self) { response in
-            switch response.result {
-            case .success(let apiResponse):
-                completion(.success(apiResponse.result))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
+import Combine
+class ApiService {
+    private static let apiKey = "a561958487d442b893570641241805"
+       private static let baseURL = "https://api.weatherapi.com/v1/forecast.json"
+      
+       static func fetchWeather( lat: String, lon: String, completion: @escaping (Result<Weather, Error>) -> Void) {
+           let parameters: [String: String] = [
+               "key": apiKey,
+               "q": "\(lat),\(lon)",
+               "days": "3",
+               "aqi": "yes",
+               "alerts": "no"
+           ]
+           
+           AF.request(baseURL, parameters: parameters).validate().responseDecodable(of: Weather.self) { response in
+               switch response.result {
+               case .success(let weatherData):
+                   completion(.success(weatherData))
+               case .failure(let error):
+                   completion(.failure(error))
+               }
+           }
+       }
 }
 
